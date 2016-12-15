34541115fa521ca7b9257728684a15e07b08f3ff
kaestel <martin@think.dk>
2016-12-15 22:46:38 +0100

    even more

--- _tools/install_webserver_configuration.sh
+++ _tools/install_webserver_configuration.sh
@@ -84,7 +84,7 @@ if test "$install_webserver_conf" = "Y"; then
 
 	read -s -p "Please enter your MariaDB password to enable logrotation: " db_root_password
 	# REPLACE EMAIL WITH PREVIOUSLY STATED EMAIL
-	sudo sed -i "s/password = .+/password = $db_root_password/;" /etc/mysql/debian.cnf
+	sudo sed -i "s/password = \.\+/password = $db_root_password/;" /etc/mysql/debian.cnf
 
 	echo
 	echo
