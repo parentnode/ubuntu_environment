77656d5df4a34d2ce7e60b6f8228c7503ccf8ca5
Martin Kæstel Nielsen <martin@think.dk>
2017-01-19 22:48:30 +0100

    debug

--- _tools/install_security.sh
+++ _tools/install_security.sh
@@ -13,7 +13,7 @@ if [ "$install_security" = "Y" ]; then
 
 
 	# ## CREATE DEPLOY GROUP AND USER
-	install_deploy=$(grep -E "^deploy:" /etc/group) || ""
+	install_deploy=$(grep -E "^deploy:" /etc/group || "")
 	echo "test3"
 
 #	install_deploy=1
