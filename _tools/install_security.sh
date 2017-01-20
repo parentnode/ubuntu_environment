5074c40d346f53ffeee2b839e44b0b810aa04b39
Martin Kæstel Nielsen <martin@think.dk>
2017-01-20 01:01:53 +0100

    debug

--- _tools/install_security.sh
+++ _tools/install_security.sh
@@ -47,127 +47,127 @@ if [ "$install_security" = "Y" ]; then
 		mkdir -p /home/$install_user/.ssh
 	fi
 
+
+	# IS TEMP KEY AVAILABLE
+	if [ -e "/home/$install_user/.key" ]; then
+
+		echo "Installing key"
+		echo
+		mv /home/$install_user/.key /home/$install_user/.ssh/authorized_keys
+	fi
+
+
+	# UPDATE PERMISSIONS EVERY TIME
+	chown -R $install_user:$install_user /home/$install_user/.ssh
+	chmod 700 /home/$install_user/.ssh
+	chmod 600 /home/$install_user/.ssh/authorized_keys
+
+
+	# UPDATE SSH PORT
+	if test -n "$install_port"; then
+
+		echo
+		echo "Updating port to: $install_port"
+		# SSH CONFIG
+		sed -i "s/Port\ [0-9]\+/Port\ $install_port/;" /etc/ssh/sshd_config
+
+	fi
+
+	# UPDATE ADDITIONAL SETTING
+	sed -i 's/PermitRootLogin\ yes/PermitRootLogin\ no/;' /etc/ssh/sshd_config
+	sed -i 's/PasswordAuthentication\ yes/PasswordAuthentication\ no/;' /etc/ssh/sshd_config
+	sed -i 's/X11Forwarding yes/X11Forwarding no/;' /etc/ssh/sshd_config
+	sed -i 's/UsePAM no/UsePAM yes/;' /etc/ssh/sshd_config
+
+
+	# WAS THE NO DNS STATEMENT ADDED
+	install_no_dns=$(grep -E "^UseDNS no$" /etc/ssh/sshd_config)
+	if [ -z "$install_no_dns" ]; then
+		echo "Adding: UseDNS no"
+
+		echo "" >> /etc/ssh/sshd_config
+		echo "UseDNS no" >> /etc/ssh/sshd_config
+	fi
+
+
+	# ADD ALLOWUSERS STATMENT
+	install_ssh_allowed_users=$(grep -E "^AllowUsers" /etc/ssh/sshd_config)
+	if test -z "$install_ssh_allowed_users"; then
+		echo
+		echo "Adding AllowUsers and $install_user to sshd_config"
+		echo "AllowUsers "$install_user >> /etc/ssh/sshd_config
+
+	# USERS ARE ALLOWED
+	else
+
+		# IS CURRENT USER ALLOWED
+		install_ssh_user=$(grep -E "^AllowUsers.*[\ ]$install_user(\ |$)" /etc/ssh/sshd_config)
+		if test -z "$install_ssh_user"; then
+			echo
+			echo "Adding $install_user to AllowUsers"
+			echo "$install_ssh_allowed_users $install_user" >> /etc/ssh/sshd_config
+		fi
+
+	fi
+
+
+
+	if [ ! -e "/etc/iptables.up.rules" ]; then
+
+
+		echo
+		echo "Copying default rules for IP TABLES"
+
+		cp /srv/tools/_conf/iptables.rules /etc/iptables.up.rules
+
+
+
+	fi
+
+	if test -n "$install_port"; then
+
+		echo
+		echo "Updating SSH port access"
+		sed -i "s/NEW\ -m\ tcp\ --dport\ [0-9]\+/NEW\ -m\ tcp\ --dport\ $install_port/;" /etc/iptables.up.rules
+
+	fi
+
+
+	echo
+	echo "Checking IP TABLES"
+	echo
+
+	# RESTORE IPTABLES
+	iptables-restore < /etc/iptables.up.rules
+
+	# SHOW IPTABLES
+	iptables -L
+
+	# SAVE NEW IPTABLES
+	iptables-save > /etc/iptables.up.rules
+
+
+	if [ ! -e "/etc/network/if-pre-up.d/iptables" ]; then
+
+		echo "Enable IP TABLE on boot"
+		echo
+
+		# LOAD ON BOOT
+		echo "#!/bin/sh" >> /etc/network/if-pre-up.d/iptables
+		echo "/sbin/iptables-restore < /etc/iptables.up.rules" >> /etc/network/if-pre-up.d/iptables
+		chmod +x /etc/network/if-pre-up.d/iptables
+
+	fi
+
+
+	echo
+	echo "Restarting SSH service"
+	echo
+	echo
+
 	#
-	# # IS TEMP KEY AVAILABLE
-	# if [ -e "/home/$install_user/.key" ]; then
-	#
-	# 	echo "Installing key"
-	# 	echo
-	# 	mv /home/$install_user/.key /home/$install_user/.ssh/authorized_keys
-	# fi
-	#
-	#
-	# # UPDATE PERMISSIONS EVERY TIME
-	# chown -R $install_user:$install_user /home/$install_user/.ssh
-	# chmod 700 /home/$install_user/.ssh
-	# chmod 600 /home/$install_user/.ssh/authorized_keys
-	#
-	#
-	# # UPDATE SSH PORT
-	# if test -n "$install_port"; then
-	#
-	# 	echo
-	# 	echo "Updating port to: $install_port"
-	# 	# SSH CONFIG
-	# 	sed -i "s/Port\ [0-9]\+/Port\ $install_port/;" /etc/ssh/sshd_config
-	#
-	# fi
-	#
-	# # UPDATE ADDITIONAL SETTING
-	# sed -i 's/PermitRootLogin\ yes/PermitRootLogin\ no/;' /etc/ssh/sshd_config
-	# sed -i 's/PasswordAuthentication\ yes/PasswordAuthentication\ no/;' /etc/ssh/sshd_config
-	# sed -i 's/X11Forwarding yes/X11Forwarding no/;' /etc/ssh/sshd_config
-	# sed -i 's/UsePAM no/UsePAM yes/;' /etc/ssh/sshd_config
-	#
-	#
-	# # WAS THE NO DNS STATEMENT ADDED
-	# install_no_dns=$(grep -E "^UseDNS no$" /etc/ssh/sshd_config)
-	# if [ -z "$install_no_dns" ]; then
-	# 	echo "Adding: UseDNS no"
-	#
-	# 	echo "" >> /etc/ssh/sshd_config
-	# 	echo "UseDNS no" >> /etc/ssh/sshd_config
-	# fi
-	#
-	#
-	# # ADD ALLOWUSERS STATMENT
-	# install_ssh_allowed_users=$(grep -E "^AllowUsers" /etc/ssh/sshd_config)
-	# if test -z "$install_ssh_allowed_users"; then
-	# 	echo
-	# 	echo "Adding AllowUsers and $install_user to sshd_config"
-	# 	echo "AllowUsers "$install_user >> /etc/ssh/sshd_config
-	#
-	# # USERS ARE ALLOWED
-	# else
-	#
-	# 	# IS CURRENT USER ALLOWED
-	# 	install_ssh_user=$(grep -E "^AllowUsers.*[\ ]$install_user(\ |$)" /etc/ssh/sshd_config)
-	# 	if test -z "$install_ssh_user"; then
-	# 		echo
-	# 		echo "Adding $install_user to AllowUsers"
-	# 		echo "$install_ssh_allowed_users $install_user" >> /etc/ssh/sshd_config
-	# 	fi
-	#
-	# fi
-	#
-	#
-	#
-	# if [ ! -e "/etc/iptables.up.rules" ]; then
-	#
-	#
-	# 	echo
-	# 	echo "Copying default rules for IP TABLES"
-	#
-	# 	cp /srv/tools/_conf/iptables.rules /etc/iptables.up.rules
-	#
-	#
-	#
-	# fi
-	#
-	# if test -n "$install_port"; then
-	#
-	# 	echo
-	# 	echo "Updating SSH port access"
-	# 	sed -i "s/NEW\ -m\ tcp\ --dport\ [0-9]\+/NEW\ -m\ tcp\ --dport\ $install_port/;" /etc/iptables.up.rules
-	#
-	# fi
-	#
-	#
-	# echo
-	# echo "Checking IP TABLES"
-	# echo
-	#
-	# # RESTORE IPTABLES
-	# iptables-restore < /etc/iptables.up.rules
-	#
-	# # SHOW IPTABLES
-	# iptables -L
-	#
-	# # SAVE NEW IPTABLES
-	# iptables-save > /etc/iptables.up.rules
-	#
-	#
-	# if [ ! -e "/etc/network/if-pre-up.d/iptables" ]; then
-	#
-	# 	echo "Enable IP TABLE on boot"
-	# 	echo
-	#
-	# 	# LOAD ON BOOT
-	# 	echo "#!/bin/sh" >> /etc/network/if-pre-up.d/iptables
-	# 	echo "/sbin/iptables-restore < /etc/iptables.up.rules" >> /etc/network/if-pre-up.d/iptables
-	# 	chmod +x /etc/network/if-pre-up.d/iptables
-	#
-	# fi
-	#
-	#
-	# echo
-	# echo "Restarting SSH service"
-	# echo
-	# echo
-	#
-	# #
-	# # # RESTART SSH
-	# service ssh restart
+	# # RESTART SSH
+	service ssh restart
 
 else
 
