05dad7b64f3acf4850e1535da4563fef2e6f794a
Martin Kæstel Nielsen <martin@think.dk>
2017-01-21 22:47:07 +0100

    let attempt

--- _tools/install_mail.sh
+++ _tools/install_mail.sh
@@ -10,7 +10,8 @@ echo
 if test "$install_mail" = "Y"; then
 
 	echo
-	echo "This is only for sending system notification mails from this server. It does not use a valid email-address for sending."
+	echo "This is only for sending system notification mails from this server."
+	echo "It does not use a valid email-address for sending and cannot be used for regular emailing."
 	echo
 	echo "Install should autofill values, but choose \"Internet Site\" if prompted for setup type."
 	echo
@@ -27,37 +28,42 @@ if test "$install_mail" = "Y"; then
 		sed -i 's/inet_interfaces = loopback-only/inet_interfaces = localhost/;' /etc/postfix/main.cf
 		sed -i 's/inet_interfaces = all/inet_interfaces = localhost/;' /etc/postfix/main.cf
 
-	fi
 
-	if [ -e "/etc/aliases" ]; then
+		if [ -e "/etc/aliases" ]; then
 
-		# update aliases
-		echo "Updating aliases"
+			# update aliases
+			echo "Updating aliases"
+			echo
+
+			sudo chmod 777 -R /etc/aliases
+
+			if ! grep -R "root:" "/etc/aliases"; then
+				echo "root:	$install_email" >> /etc/aliases
+			fi
+
+			if ! grep -R "$install_user:" "/etc/aliases"; then
+				echo "$install_user:	$install_email" >> /etc/aliases
+			fi
+
+			sudo chmod 644 -R /etc/aliases
+
+			sudo newaliases
+
+		fi
+
+		# restart mail service
+		sudo service postfix restart
+
+
+		echo "Your email was configured correctly" | mail -s "Linux server email setup" $install_email
+		echo
 		echo
 
-		sudo chmod 777 -R /etc/aliases
+	else
 
-		if ! grep -R "root:" "/etc/aliases"; then
-			echo "root:	$install_email" >> /etc/aliases
-		fi
-
-		if ! grep -R "$install_user:" "/etc/aliases"; then
-			echo "$install_user:	$install_email" >> /etc/aliases
-		fi
-
-		sudo chmod 644 -R /etc/aliases
-
-		sudo newaliases
+		echo "Setting up mail tools failed!"
 
 	fi
-
-	# restart mail service
-	sudo service postfix restart
-
-
-	echo "Your email was configured correctly" | mail -s "Linux server email setup" $install_email
-	echo
-	echo
 
 
 else
