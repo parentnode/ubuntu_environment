2fad7760fac421f0b1c762d72d97e2b381c5c09e
Martin Kæstel Nielsen <martin@think.dk>
2017-01-24 14:43:12 +0100

    Testing PHP7.1

--- _tools/install_software.sh
+++ _tools/install_software.sh
@@ -17,16 +17,25 @@ if test "$install_software" = "Y"; then
 	# aptitude install landscape-client landscape-common
 
 	# INSTALL APACHE
-	apt install -y apache2 apache2-utils ssl-cert
+	sudo apt install -y apache2 apache2-utils ssl-cert
 
-	# INSTALL PHP
-	apt install -y libapache2-mod-php php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-dev php-imagick php7.0-mcrypt php7.0-mbstring php7.0-zip php-memcached php7.0-mysql php7.0-xmlrpc memcached
+	sudo add-apt-repository -y ppa:ondrej/php
+	sudo apt update -y
+
+	# INSTALL PHP7.1
+	sudo apt install -y libapache2-mod-php php7.1 php7.1-cli php7.1-common php7.1-curl php7.1-dev php-imagick php7.1-mcrypt php7.1-mbstring php7.1-zip php-memcached php7.1-mysql php7.1-xmlrpc memcached
+
+	# INSTALL PHP7.0
+	# apt install -y libapache2-mod-php php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-dev php-imagick php7.0-mcrypt php7.0-mbstring php7.0-zip php-memcached php7.0-mysql php7.0-xmlrpc memcached
+
+	# INSTALL PHP5.6
+	# sudo apt install -y libapache2-mod-php php5.6 php5.6-cli php5.6-common php5.6-curl php5.6-dev php-imagick php5.6-mcrypt php5.6-mbstring php5.6-zip php-memcached php5.6-mysql php5.6-xmlrpc memcached
 
 	# INSTALL ZIP, LOG ROTATION, CURL
-	apt install -y zip logrotate curl
+	sudo apt install -y zip logrotate curl
 
 	# INSTALL mariaDB
-	apt install -y mariadb-server
+	sudo apt install -y mariadb-server
 
 
 	echo
