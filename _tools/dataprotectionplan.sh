c0556ebf42d2858d81f6a2f67de1719757b65e93
Martin Kæstel Nielsen <martin@think.dk>
2017-04-15 17:22:01 +0200

    Fixed flawed test

--- _tools/dataprotectionplan.sh
+++ _tools/dataprotectionplan.sh
@@ -12,7 +12,10 @@ echo "- make zipped tarball"
 echo "- encrypt data"
 echo "- send as email"
 
-if [ -d "/srv/crons/conf/db/$1" ]; then
+echo "/srv/crons/conf/db/$1";
+
+
+if [ -e "/srv/crons/conf/db/$1" ]; then
 
 	# read username and password from conf file
 	config=$(<"/srv/crons/conf/db/$1")
@@ -30,7 +33,7 @@ if [ -d "/srv/crons/conf/db/$1" ]; then
 	# decryption done by
 	# openssl aes-128-cbc -d < yourfile.txt.aes > yourfile.txt
 
-	echo "You are welcome :)" | mail -s "DATA PROTECTION PLAN FOR $1" -A $1.sql.tar.gz.aes $4
+	echo "You are welcome :)" | mail -s "DATA PROTECTION PLAN FOR $1" -A $1.sql.tar.gz.aes $2
 
 
 	# clean up
