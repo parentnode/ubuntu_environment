e00337c5198414e910f25a0489cae6b9fdaf6093
Martin Kæstel Nielsen <martin@think.dk>
2018-06-01 08:09:33 +0200

    Update client setup conf

--- scripts/install_wkhtmlto.sh
+++ scripts/install_wkhtmlto.sh
@@ -12,7 +12,8 @@ echo
 if test "$install_wkhtml" = "Y"; then
 
 	# WKHTML - FORCE PASSWORD RENEWAL
-	echo "Using /srv/tools/bin"
+	echo "Using /srv/tools/bin/wkhtmltopdf"
+
 	# sudo apt install -y wkhtmltopdf
 	# sudo cp /srv/tools/conf/wkhtmltoimage /usr/bin/static_wkhtmltoimage
 	# sudo cp /srv/tools/conf/wkhtmltopdf /usr/bin/static_wkhtmltopdf
