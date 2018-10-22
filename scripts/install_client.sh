994d6fb40d0968a5db8ec8f385e18a2e1101dbe0
David Jessen <jessen-it@outlook.com>
2018-10-22 14:38:56 +0200

    Update scripts/install_client.sh

--- scripts/install_client.sh
+++ scripts/install_client.sh
@@ -59,7 +59,10 @@ echo "Supply password"
 
 #Checks if root password are set
 mysqlstatus=$(dpkg --get-selections | grep mysql)
+export mysqlstatus
+
 dbstatus=$(sudo mysql --user=root -e exit 2>/dev/null || echo 1)
+export dbstatus
 
 #echo "dbstatus: $dbstatus"
 #echo "Mysql status: $mysqlstatus"
