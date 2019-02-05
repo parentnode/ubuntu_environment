71f8a733b8da6b3b1fa83444da0f6fe74ad05e93
David Jessen <jessen-it@outlook.com>
2019-02-05 15:04:51 +0100

    Update install_php.sh

--- scripts/install_php.sh
+++ scripts/install_php.sh
@@ -39,8 +39,14 @@ echo
 #cat /srv/tools/conf-client/php-apache2.ini > /etc/php/7.1/apache2/php.ini
 #cat /srv/tools/conf-client/php-cli.ini > /etc/php/7.1/cli/php.ini
 
+echo
+echo "Copying contents from parentnode apache2.ini into /etc/php/7.2/apache2/php.ini"
+echo
 # PHP 7.2
 cat /srv/tools/conf-client/php-apache2.ini > /etc/php/7.2/apache2/php.ini
+echo
+echo "Copying contents from parentnode php-cli.ini into /etc/php/7.2/cli/php.ini"
+echo
 cat /srv/tools/conf-client/php-cli.ini > /etc/php/7.2/cli/php.ini
 echo
 
