6f9c105b4fdac97ac1dc376c31539d70991136f4
David <jessen-it@outlook.com>
2019-02-07 15:09:13 +0100

    changing order in installing to older logic

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -9,7 +9,6 @@ echo
 
 if test "$install_software" = "Y"; then
 
-
 	echo
 	echo "Installing software"
 	echo
@@ -91,7 +90,7 @@ if test "$install_software" = "Y"; then
 		echo
 		echo
 		# RESTART APACHE
-		#sudo service apache2 restart
+		service apache2 restart
 
 		echo
 		echo "Installing and configuring mariadb"
