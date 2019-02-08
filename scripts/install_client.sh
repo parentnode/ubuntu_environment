b5d298181ceedb8ce0b0b898905fbc2250ada9bb
David <jessen-it@outlook.com>
2019-02-08 15:07:27 +0100

    Commentary and new functions

--- scripts/install_client.sh
+++ scripts/install_client.sh
@@ -36,7 +36,7 @@ guiText "Please enter your email for apache installation" "Comment"
 read -p "Your email address: " install_email
 export install_email
 
-guiText ".bash profile are loaded in to your promt when you start ubuntu" \ 
+guiText ".bash profile are loaded in to your promt when you start ubuntu" \
 "Comment" \
 "when this installer are done it will change color and when you are entering an git repository"
 
