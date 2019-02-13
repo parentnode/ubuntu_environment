2921aa998049ee14962889cee7eef5f626172e08
jessen1989 <jessen-it@outlook.com>
2019-02-13 10:25:36 +0100

    fixed string escapes

--- scripts/install_promt.sh
+++ scripts/install_promt.sh
@@ -12,14 +12,14 @@ fi
 
 checkFileContent "/home/$install_user/.bash_profile" "/srv/tools/conf-client/dot_bash_profile"
 
-"if [ -f \"$HOME/.bash_profile\" ]; then . \"\$HOME/.bash_profile\" fi"
-install_bash_profile=$(grep -E "if [ -f \"\$HOME/.bash_profile\" ]; then . \"\$HOME/.bash_profile\" fi" /$HOME/.bashrc || echo "")
+
+install_bash_profile=$(grep -E "if [ -f \"\"$HOME\"/.bash_profile\" ]; then . \"\"$HOME\"/.bash_profile\" fi" /$HOME/.bashrc || echo "")
 #install_bash_profile=$(grep -E "\$HOME\/\.bash_profile" /home/$install_user/.bashrc || echo "")
 if [ -z "$install_bash_profile" ]; then
 
 	# Add .bash_profile to .bashrc
 	echo
-	echo "if [ -f \"\$HOME/.bash_profile\" ]; then" >> /home/$install_user/.bashrc
-	echo " . \"\$HOME/.bash_profile\"" >> /home/$install_user/.bashrc
-	echo "fi" >> /home/$install_user/.bashrc
+	echo "if [ -f \"$HOME/.bash_profile\" ]; then" >> /$HOME/.bashrc
+	echo " \. $HOME/.bash_profile" >> $HOME/.bashrc
+	echo "fi" >> $HOME/.bashrc
 fi
