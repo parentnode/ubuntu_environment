40b68b3b75c807d8d763241ef23389b7dd752bf1
jessen1989 <jessen-it@outlook.com>
2019-02-13 10:30:34 +0100

    fixed string escapes

--- scripts/install_promt.sh
+++ scripts/install_promt.sh
@@ -13,7 +13,7 @@ fi
 checkFileContent "/home/$install_user/.bash_profile" "/srv/tools/conf-client/dot_bash_profile"
 
 
-install_bash_profile=$(grep -E "if [ -f \"\"$HOME\"/.bash_profile\" ]; then . \"\"$HOME\"/.bash_profile\" fi" /$HOME/.bashrc || echo "")
+install_bash_profile=$(grep -E "if [ -f \"$HOME/.bash_profile\" ]; then \. \"$HOME/.bash_profile\" fi" /$HOME/.bashrc || echo "")
 #install_bash_profile=$(grep -E "\$HOME\/\.bash_profile" /home/$install_user/.bashrc || echo "")
 if [ -z "$install_bash_profile" ]; then
 
