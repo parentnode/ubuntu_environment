99d900d3c5514ad4a1af3eb2c443756549cc11d5
jessen1989 <jessen-it@outlook.com>
2019-02-18 11:39:16 +0100

    testing apache mail check

--- scripts/install_client.sh
+++ scripts/install_client.sh
@@ -44,6 +44,8 @@ then
 		export install_email
 	else 
 		guiText "Apache email" "Installed"
+		install_email = $install_email
+		export install_email
 	fi
 else
 	guiText "Please enter your email for apache installation" "Comment"
