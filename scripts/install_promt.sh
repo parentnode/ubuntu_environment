851da7081158a2a38355a257f23ee8e16a0c76c9
jessen1989 <jessen-it@outlook.com>
2019-02-18 12:56:34 +0100

    testing apache mail check

--- scripts/install_promt.sh
+++ scripts/install_promt.sh
@@ -10,8 +10,15 @@
 #	sudo cp /srv/tools/conf-client/default_conf_complete /$HOME/.bash_profile
 #fi
 guiText "Existing .bash_profile" "Replace" "parentnode promt"
-updateStatementInFile "enable_git_prompt" "/srv/tools/conf-client/default_conf_complete" "$HOME/.bash_profile"
-updateStatementInFile "alias" "/srv/tools/conf-client/default_conf_complete" "$HOME/.bash_profile"
+does_parentnode_git_exist=$(grep -E "enable_git_prompt" $HOME/.bash_profile || echo "")
+does_parentnode_alias_exist=$(grep -E "alias" $HOME/.bash_profile || echo "")
+if [ -z "$does_parentnode_git_exist" ] || [ -z "$does_parentnode_alias_exist" ];
+then
+	sudo cp /srv/tools/conf-client/default_conf_complete /$HOME/.bash_profile
+else
+	updateStatementInFile "enable_git_prompt" "/srv/tools/conf-client/default_conf_complete" "$HOME/.bash_profile"
+	updateStatementInFile "alias" "/srv/tools/conf-client/default_conf_complete" "$HOME/.bash_profile"
+fi
 #checkAlias "/home/$install_user/.bash_profile" "/srv/tools/conf-client/dot_bash_profile"
 #checkStringInFile "export PS1" "$HOME/.bash_profile"
 
