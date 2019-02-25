cd75b4584e33a2588fefb639c3d980ab0806503b
jessen1989 <jessen-it@outlook.com>
2019-02-25 17:03:56 +0100

    reverted back to older mariadb installation

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -251,12 +251,7 @@ installedPackage(){
 			sudo -$2 apt install $1 -y
 		fi
 		if [ "$1" == "mariadb-server" ]; then
-			sudo apt-get install software-properties-common
-			sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-			sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirrors.dotsrc.org/mariadb/repo/10.2/ubuntu bionic main'
-			sudo apt update
 			sudo -$2 apt install -$3 $1 -y
-			sudo mysql_secure_installation "$db_root_password"
 		fi
 	else 
 		guiText "$1" "Installed"
