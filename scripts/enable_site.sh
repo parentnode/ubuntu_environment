f652cbdc9dc60e61dc0b9ff1e939fec0ce3a0423
jessen1989 <jessen-it@outlook.com>
2019-03-08 11:56:44 +0100

    added file permissions to enable site

--- scripts/enable_site.sh
+++ scripts/enable_site.sh
@@ -100,7 +100,7 @@ if [ -e "$PWD/apache/httpd-vhosts.conf" ] ; then
 			echo "Project already enabled in $host_file_path"
 
 		fi
-
+		sudo chown -R www-data:staff "$document_root"
 		# Restart apache after modification
 		echo ""
 		echo "Restarting Apache"
