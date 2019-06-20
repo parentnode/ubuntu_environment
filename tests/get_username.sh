ead38c7e8d2243d39167f4e0cfbbe6cad287c793
jessen1989 <jessen-it@outlook.com>
2019-06-20 12:23:17 +0200

    added getuser test

--- tests/get_username.sh
+++ tests/get_username.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
-source /srv/sites/parentnode/windows_environment/scripts/functions.sh
+source /srv/sites/parentnode/ubuntu_environment/scripts/functions.sh
 username="$(getUsername)"
 echo "Current user is: $username"
