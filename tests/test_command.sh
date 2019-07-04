afe0919f38c6b29edae24356df6ed24ce784c98e
jessen1989 <jessen-it@outlook.com>
2019-07-04 10:15:06 +0200

    added testCommand test

---
+++ tests/test_command.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+source /srv/sites/parentnode/ubuntu_environment/scripts/functions.sh
+# Check if program/service are installed
+echo "Testing testCommand"
+echo 
+valid_status=("active (running)" "inactive (dead)")
+echo "Checking Apache2.4 status: "
+testCommand "service apache2 status" "${valid_status[@]}"
+echo
+echo "Checking Redis status: "
+testCommand "service redis status" "${valid_status[@]}"
+echo
+echo "Checking unzip version: "
+valid_version=("^UnZip ([6\.[0-9])")
+testCommand "unzip -v" "${valid_version[@]}"
+
+# Usage: returns a true if a program or service are located in the installed services or programs
+# P1: kommando
+# P2: array of valid responses
