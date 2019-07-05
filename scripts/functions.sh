3eea8cb7b06933b8db8b56e96779d1dfc48018d7
jessen1989 <jessen-it@outlook.com>
2019-07-05 12:39:44 +0200

    added testGitCredential

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -119,6 +119,13 @@ testCommand(){
 }
 export -f testCommand
 
+checkGitCredential(){
+	value=$(git config user.$1)
+	echo "$value"
+
+}
+export -f checkGitCredential
+
 guiText(){
 	# Automatic comment format for simple setup as a text based gui
 	# eg. guiText "Redis" "Start"
