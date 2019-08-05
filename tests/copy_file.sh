074a94021e7154ba26c914381529316b89aad3e1
jessen1989 <jessen-it@outlook.com>
2019-08-05 14:29:53 +0200

    added file_exist.sh

--- tests/copy_file.sh
+++ tests/copy_file.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 source /srv/sites/parentnode/ubuntu_environment/scripts/functions.sh
 copyFile "/srv/sites/parentnode/ubuntu_environment/tests/ask.sh" "/srv/sites/parentnode/ubuntu_environment/tests/copied_ask.sh"
+copyFile "/srv/sites/parentnode/ubuntu_environment/tests/ask.sh" "$HOME/Desktop"
 
--- tests/file_exists.sh
+++ tests/file_exists.sh
@@ -1,2 +1,8 @@
 #!/bin/bash -e
 source /srv/sites/parentnode/ubuntu_environment/scripts/functions.sh
+if [ "$(fileExists "$HOME/.bash_profile")" = "true" ]; then 
+    echo "file exists"
+else
+    echo "file do not exists"
+fi
+
