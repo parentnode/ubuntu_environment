9f8377dd1543522a7beb019dd301f7e5037526c4
jessen1989 <jessen-it@outlook.com>
2019-08-05 11:00:25 +0200

    added copyFile.sh

---
+++ tests/copy_file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+source /srv/sites/parentnode/ubuntu_environment/scripts/functions.sh
+copyFile "/srv/sites/parentnode/ubuntu_environment/tests/ask.sh" "/srv/sites/parentnode/ubuntu_environment/tests/copied_ask.sh"
+
