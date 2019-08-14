e8e5194df9ade98d72cfa1add0280cc4d212ef13
jessen1989 <jessen-it@outlook.com>
2019-08-14 13:41:03 +0200

    testing check_directories.sh

---
+++ scripts/checking_directories.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+#create_folder_if_no_exist
+checkFolderExistOrCreate "/srv/sites"
+checkFolderExistOrCreate "/srv/sites/apache"
+checkFolderExistOrCreate "/srv/sites/apache/logs"
+checkFolderExistOrCreate "/srv/sites/parentnode"
+
+outputHandler "comment" "Changing Folder rights from root to your curent user"
+# Change Folder Rights from root to current user
+chown -R $SUDO_USER:$SUDO_USER /srv/sites
