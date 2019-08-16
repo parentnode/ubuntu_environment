9e249d7477453f92ba6d7e6f19aef153f5415e2d
jessen1989 <jessen-it@outlook.com>
2019-08-16 10:24:59 +0200

    testing functions in install script install software zip

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -78,7 +78,7 @@ if test "$install_software" = "Y"; then
 	fi 
 	outputHandler "section" "Installing Log Rotation"
 	valid_version="^logrotate ([3\.[0-9])"
-	if [ -z "$(testCommand "logrotate -v" "${valid_version[@]}")" ]; then 
+	if [ -z "$(testCommand "logrotate --version" "${valid_version[@]}")" ]; then 
 		command "sudo apt install -y logrotate" 
 	else
 		outputHandler "comment" "logrotate Installed" "[logrotate Version:] $(testCommand "logrotate -v" "${valid_version[@]}")"
