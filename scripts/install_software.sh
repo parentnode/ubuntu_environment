9af2d1200a615cc3b9f984b5605da156750135b4
jessen1989 <jessen-it@outlook.com>
2019-08-16 11:37:12 +0200

    testing functions in install script install software logrotate

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -69,13 +69,13 @@ if test "$install_software" = "Y"; then
 	
 
 	#installedPackage "redis"
-	outputHandler "section" "Installing Zip"
-	valid_version="Zip ([3\.[0-9])"
-	if [ -z "$(testCommand "zip -v" "${valid_version[@]}")" ]; then 
-		command "sudo apt install -y zip"
-	else
-		outputHandler "comment" "Zip Installed" "[Zip Version:] $(testCommand "zip -v" "${valid_version[@]}")"
-	fi 
+	#outputHandler "section" "Installing Zip"
+	#valid_version="Zip ([3\.[0-9])"
+	#if [ -z "$(testCommand "zip -v" "${valid_version[@]}")" ]; then 
+	#	command "sudo apt install -y zip"
+	#else
+	#	outputHandler "comment" "Zip Installed" "[Zip Version:] $(testCommand "zip -v" "${valid_version[@]}")"
+	#fi 
 	outputHandler "section" "Installing Log Rotation"
 	valid_version="logrotate ([3\.[1-9]])"
 	if [ -z "$(testCommand "logrotate -v" "${valid_version[@]}")" ]; then
