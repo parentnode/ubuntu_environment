9ea2199c54b886d63db35969f6c53619f6311e36
jessen1989 <jessen-it@outlook.com>
2019-08-22 10:14:00 +0200

    added comments to replace_apache_default_mail.sh

--- tests/replace_apache_default_mail.sh
+++ tests/replace_apache_default_mail.sh
@@ -1,17 +1,22 @@
 #!/bin/bash -e
+# This is a test for replacing whatever value at the ServerAdmin section in apache.conf with a valid email
 source /srv/sites/parentnode/ubuntu_environment/scripts/functions.sh
-install_email="jessen-it@outlook.com"
-#apache_email=$(grep "ServerAdmin" /srv/sites/parentnode/ubuntu_environment/tests/test_replace_default_mail/email_file | cut -d' ' -f10 || echo "")
-grep_apache_email=$(trimString "$(grep "ServerAdmin" /srv/sites/parentnode/ubuntu_environment/tests/test_replace_default_mail/email_file)")
-apache_email=$(echo "$grep_apache_email" | cut -d' ' -f2)
-#echo "$apache_email"
 
+
+install_email="mail@outlook.com"
+
+#Catching the line where keyword is ServerAdmin trimming the string for leading and trailing spaces 
+grep_apache_email=$(trimString "$(grep "ServerAdmin" /srv/sites/parentnode/ubuntu_environment/tests/test_replace_default_mail_apache/apache_test_file)")
+#Seperate the line at the first space in to parts and saving the second part in a variable ($apache_email) here is the mail normally located
+apache_email=$(echo "$grep_apache_email" | cut -d' ' -f2)
+
+# If $apache_email is replace the ServerAdmin line with a new one containing the mail in $install_mail
 if [ -z "$apache_email" ]; then
     echo "trying to set mail"
-    sed -i "s/ServerAdmin\ /ServerAdmin $install_email/" /srv/sites/parentnode/ubuntu_environment/tests/test_replace_default_mail/email_file
+    sed -i "s/ServerAdmin\ /ServerAdmin $install_email/" /srv/sites/parentnode/ubuntu_environment/tests/test_replace_default_mail_apache/apache_test_file
 fi
-#echo "$apache_email"
+# After a fresh installation of apache the ServerAdmin gets a placeholder mail to work called webmaster@local,
+# here we replace that email with $apache_email
 if [ "$apache_email" = "webmaster@localhost" ]; then
-    sed -i "s/webmaster@localhost/$install_email/" /srv/sites/parentnode/ubuntu_environment/tests/test_replace_default_mail/email_file
-    #echo "Test $apache_email"
+    sed -i "s/webmaster@localhost/$install_email/" /srv/sites/parentnode/ubuntu_environment/tests/test_replace_default_mail_apache/apache_test_file
 fi
