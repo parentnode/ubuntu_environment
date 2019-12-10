fdd20d8c502a54a7630d98e0bdb7c687e60f1071
jessen1989 <jessen-it@outlook.com>
2019-12-10 08:07:13 +0100

    added ask for port number ubuntu server version 1.0

--- scripts/check_software_prerequisites_is_met_server.sh
+++ scripts/check_software_prerequisites_is_met_server.sh
@@ -84,7 +84,7 @@ if test "$install_security" = "Y"; then
 	port_number=$(grep -E "^Port\ ([0-9]+)$" /etc/ssh/sshd_config | sed "s/Port //;")
 	#read -p "Specify SSH port (leave empty to keep $port_number): " install_port
 	port_array=("[0-9]{2,6}")
-	if [ -z "$port_number" ]; then
+	if [ -n "$port_number" ]; then
 		install_port=$(ask "Specify SSH port" "${port_array[@]}" "port")
 	else
 		install_port=$(ask "Specify SSH port (leave empty to keep $port_number)" "${port_array[@]}" "port") || echo ""
