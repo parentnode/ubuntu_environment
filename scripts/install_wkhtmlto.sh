4e8987068f551ad9424a52ce2d242f3ea80036f7
jessen1989 <jessen-it@outlook.com>
2019-12-17 13:23:37 +0100

    install software

--- scripts/install_wkhtmlto.sh
+++ scripts/install_wkhtmlto.sh
@@ -1,27 +1,17 @@
 #!/bin/bash -e
-
-echo "-----------------------------------------"
-echo
-echo "                 WKHTML"
-echo
-echo
-echo "      http://wkhtmltopdf.org - LGPLv3"
-echo
-
+outputHandler "section" "WKHTML"
+outputHandler "comment" "http://wkhtmltopdf.org - LGPLv3"
 
 if test "$install_wkhtml" = "Y"; then
 
 	# WKHTML - FORCE PASSWORD RENEWAL
-	echo "Using /srv/tools/bin/wkhtmltopdf"
+	outputHandler "comment" "Using /srv/tools/bin/wkhtmltopdf"
 	# sudo apt install -y wkhtmltopdf
 	# installedPackage "wkhtmltopdf"
 	# sudo cp /srv/tools/conf/wkhtmltoimage /usr/bin/static_wkhtmltoimage
 	# sudo cp /srv/tools/conf/wkhtmltopdf /usr/bin/static_wkhtmltopdf
 
 else
-	guiText "SWKHTML" "Skip"
+	outputHandler "section" "skipping WKHTML"
 fi
-
-echo
-echo
 
