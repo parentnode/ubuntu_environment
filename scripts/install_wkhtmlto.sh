27dc7ffd8d5aa90e7144493cec083c628cb0431d
jessen1989 <jessen-it@outlook.com>
2020-02-24 10:16:06 +0100

    alternative to copiyng static files

--- scripts/install_wkhtmlto.sh
+++ scripts/install_wkhtmlto.sh
@@ -6,10 +6,17 @@ if test "$install_wkhtml" = "Y"; then
 
 	# WKHTML - FORCE PASSWORD RENEWAL
 	outputHandler "comment" "Using /srv/tools/bin/wkhtmltopdf"
-	sudo apt install -y wkhtmltopdf
+	#sudo apt install -y wkhtmltopdf
 	# installedPackage "wkhtmltopdf"
 	# sudo cp /srv/tools/conf/wkhtmltoimage /usr/bin/static_wkhtmltoimage
 	# sudo cp /srv/tools/conf/wkhtmltopdf /usr/bin/static_wkhtmltopdf
+	#wget https://downloads.wkhtmltopdf.org/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+	#wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+	wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+	tar -xvf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+	cd wkhtmltox/bin/
+	sudo mv wkhtmltopdf  /usr/bin/wkhtmltopdf
+	sudo mv wkhtmltoimage  /usr/bin/wkhtmltoimage
 
 else
 	outputHandler "section" "skipping WKHTML"
