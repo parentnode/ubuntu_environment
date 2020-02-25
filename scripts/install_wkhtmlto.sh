b3eb2156e868f92ccc8dc83e818b35a31e2fb529
jessen1989 <jessen-it@outlook.com>
2020-02-25 15:44:46 +0100

    temp fix test for wkhtmltopdf 0.12.5_1

--- scripts/install_wkhtmlto.sh
+++ scripts/install_wkhtmlto.sh
@@ -12,11 +12,12 @@ if test "$install_wkhtml" = "Y"; then
 	# sudo cp /srv/tools/conf/wkhtmltopdf /usr/bin/static_wkhtmltopdf
 	#wget https://downloads.wkhtmltopdf.org/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 	#wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
-	wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-	tar -xvf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-	cd wkhtmltox/bin/
-	sudo mv wkhtmltopdf  /usr/bin/wkhtmltopdf
-	sudo mv wkhtmltoimage  /usr/bin/wkhtmltoimage
+	
+	sudo wget -P Downloads https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb 
+	cd ~/Downloads
+	sudo apt install xfonts-75dpi
+	sudo dpkg -i wkhtmltox_0.12.5-1.bionic_amd64.deb
+	sudo cp wkhtmltopdf /srv/tools/bin/wkhtmltopdf
 
 else
 	outputHandler "section" "skipping WKHTML"
