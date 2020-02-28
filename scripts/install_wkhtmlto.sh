ae4aacfadbe828553cc86e960383a0c94b65c797
jessen1989 <jessen-it@outlook.com>
2020-02-28 09:52:37 +0100

    removing irrelevant quickfix wkhtmltopdf

--- scripts/install_wkhtmlto.sh
+++ scripts/install_wkhtmlto.sh
@@ -13,11 +13,12 @@ if test "$install_wkhtml" = "Y"; then
 	#wget https://downloads.wkhtmltopdf.org/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 	#wget http://download.gna.org/wkhtmltopdf/0.12/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
 	
-	sudo wget -P Downloads https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb 
-	cd ~/Downloads
-	sudo apt install xfonts-75dpi
-	sudo dpkg -i wkhtmltox_0.12.5-1.bionic_amd64.deb
-	sudo cp wkhtmltopdf /srv/tools/bin/wkhtmltopdf
+	#sudo wget -P Downloads https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.bionic_amd64.deb 
+	#cd ~/Downloads
+	#sudo apt install xfonts-75dpi
+	#sudo apt install xfonts-base
+	#sudo dpkg -i wkhtmltox_0.12.5-1.bionic_amd64.deb 
+	#sudo cp /usr/local/bin/wkhtmltopdf /srv/tools/bin/wkhtmltopdf
 
 else
 	outputHandler "section" "skipping WKHTML"
