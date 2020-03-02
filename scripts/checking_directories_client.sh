ec6bf53e060a1cc6e9c69023314e9ad2d8852bc2
jessen1989 <jessen-it@outlook.com>
2020-03-02 19:02:30 +0100

    added support for multi user ubuntu-client

--- scripts/checking_directories_client.sh
+++ scripts/checking_directories_client.sh
@@ -3,8 +3,19 @@ outputHandler "section" "Checking directories"
 #create_folder_if_no_exist
 
 checkFolderExistOrCreate "/home/$install_user/Sites"
+
 if [ ! -e "/srv/sites" ]; then 
     ln -s /home/$install_user/Sites /srv/sites
+else
+    outputHandler "Symlink exists"
+    sites_symlink_exists=$(ls -Fla /srv | grep /home/ | cut -d \/ -f3)
+    if [ "$sites_symlink_exists" = "$install_user"  ]; then
+        outputHandler "comment" "Symlink belongs to $install_user" "No need for creating a symlink"
+    else
+        rm /srv/sites
+        ln -s /home/$install_user/Sites /srv/sites
+    fi
+
 fi
 checkFolderExistOrCreate "/srv/sites/apache"
 checkFolderExistOrCreate "/srv/sites/apache/logs"
