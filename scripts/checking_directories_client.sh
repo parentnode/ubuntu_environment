c8339da3013e37397983f47b3ac237fbc705af4a
jessen1989 <jessen-it@outlook.com>
2020-03-03 09:26:47 +0100

    revert support for multiuser until troubleshooting

--- scripts/checking_directories_client.sh
+++ scripts/checking_directories_client.sh
@@ -4,27 +4,30 @@ outputHandler "section" "Checking directories"
 
 
 checkFolderExistOrCreate "/home/$install_user/Sites"
-checkFolderExistOrCreate "/home/$install_user/Sites/parentnode"
-checkFolderExistOrCreate "/srv"
-checkFolderExistOrCreate "/srv/sites"
-sites_symlink_exists=$(ls -Fla /srv/sites | grep /home/ | cut -d \/ -f3)
-
-if [ ! -e "/srv/sites/parentnode" ]; then 
-    ln -s /home/$install_user/Sites/parentnode /srv/sites/parentnode
-else
-    outputHandler "comment" "Symlink"
-    #sites_symlink_exists=$(ls -Fla /srv | grep /home/ | cut -d \/ -f3)
-    if [ "$sites_symlink_exists" = "$install_user"  ]; then
-        outputHandler "comment" "Symlink belongs to $install_user" "No need for creating a symlink"
-    else
-        rm /srv/sites/parentnode
-        ln -s /home/$install_user/Sites/parentnode /srv/sites/parentnode
-    fi
-
+#checkFolderExistOrCreate "/home/$install_user/Sites/parentnode"
+#checkFolderExistOrCreate "/srv"
+#checkFolderExistOrCreate "/srv/sites"
+#sites_symlink_exists=$(ls -Fla /srv/sites | grep /home/ | cut -d \/ -f3)
+#
+#if [ ! -e "/srv/sites/parentnode" ]; then 
+#    ln -s /home/$install_user/Sites/parentnode /srv/sites/parentnode
+#else
+#    outputHandler "comment" "Symlink"
+#    #sites_symlink_exists=$(ls -Fla /srv | grep /home/ | cut -d \/ -f3)
+#    if [ "$sites_symlink_exists" = "$install_user"  ]; then
+#        outputHandler "comment" "Symlink belongs to $install_user" "No need for creating a symlink"
+#    else
+#        rm /srv/sites/parentnode
+#        ln -s /home/$install_user/Sites/parentnode /srv/sites/parentnode
+#    fi
+#
+#fi
+if [ ! -e /srv/sites ]; then
+     ln -s /home/$install_user/Sites/parentnode /srv/sites
 fi
 checkFolderExistOrCreate "/srv/sites/apache"
 checkFolderExistOrCreate "/srv/sites/apache/logs"
-#checkFolderExistOrCreate "/srv/sites/parentnode"
+checkFolderExistOrCreate "/srv/sites/parentnode"
 checkFolderExistOrCreate "/srv/sites/apache/ssl"
 
 outputHandler "comment" "Changing Folder rights from root to your curent user"
