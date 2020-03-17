3728f98497ce8b6a3fd148cee88952c152bec90f
jessen1989 <jessen-it@outlook.com>
2020-03-17 14:31:15 +0100

    testing refactored stack

--- tests/update_content.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-source /srv/sites/parentnode/ubuntu_environment/scripts/functions.sh
-
-updateContent "#parentnode: github" "/srv/sites/parentnode/ubuntu_environment/tests/test_update_content/source" "/srv/sites/parentnode/ubuntu_environment/tests/test_update_content/destination"
