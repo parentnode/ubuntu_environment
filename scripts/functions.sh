2f07cd50024c56b137a2076d315764b5c8d85f58
jessen1989 <jessen-it@outlook.com>
2020-03-20 10:53:54 +0100

    implementing changes to .bash_profile

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -261,8 +261,8 @@ export -f trimString
 
 createOrModifyBashProfile(){
 	# if $shell_interactive have value, the computer is accessed with an login prompt normally a server
-	#shell_command=
-	if [[ "$-" == *i* ]]; then
+	server=$(dpkg -l *ubuntu-server | grep server || echo "")
+	if [ -n $server  ]; then
 		echo "server conf"
 		conf="/srv/tools/conf-server/dot_profile"
 	else
