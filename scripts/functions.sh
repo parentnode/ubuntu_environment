1fa0fd369694271e9c26a5481acbc8d3d6d2ed61
jessen1989 <jessen-it@outlook.com>
2020-03-20 01:12:35 +0100

    implementing changes to server

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -263,7 +263,7 @@ createOrModifyBashProfile(){
 	# if $shell_interactive have value, the computer is accessed with an login prompt normally a server
 	shell_command=
 	shell_interactive=$(echo "$(shopt login_shell | grep on)")
-	if [ -z "$shell_interactive" ]; then
+	if [ -z $shell_interactive ]; then
 		echo "client conf"
 		conf="/srv/tools/conf-client/default_conf_complete"
 		#shell="$HOME/.profile"
