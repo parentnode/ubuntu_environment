823cbde861d1606699f2d99252962e9db0b18856
jessen1989 <jessen-it@outlook.com>
2020-03-24 16:32:37 +0100

    testing to fix enable_site

--- scripts/enable_site_test.sh
+++ scripts/enable_site_test.sh
@@ -38,7 +38,7 @@ setHost(){
 	if [ -z "$host_exist" ]; then 
 		#setHost "$1"
 		echo "Setting up $1 host"
-		echo "127.0.0.1$'\t'"$1"" >> "$host_file_path"
+		echo "127.0.0.1		"$1"" >> "$host_file_path"
 	else 
 		echo "$1 exists"	
 	fi
