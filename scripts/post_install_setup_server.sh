4f21f7eff32328fe8d582d541bb9605d6663d4b0
jessen1989 <jessen-it@outlook.com>
2020-04-30 09:01:44 +0200

    retrieving php7.4 ini's

--- scripts/post_install_setup_server.sh
+++ scripts/post_install_setup_server.sh
@@ -76,12 +76,12 @@ if test "$install_webserver_conf" = "Y"; then
 	# PHP 7.1
 	#cat /srv/tools/conf-server/php-apache2.ini > /etc/php/7.1/apache2/php.ini
 	#cat /srv/tools/conf-server/php-cli.ini > /etc/php/7.1/cli/php.ini
-	outputHandler "comment" "setting up apache2.ini"
+	#outputHandler "comment" "setting up apache2.ini"
     # PHP 7.2
-    cat /srv/tools/conf-server/php-apache2.ini > /etc/php/7.2/apache2/php.ini
+    #cat /srv/tools/conf-server/php-apache2.ini > /etc/php/7.2/apache2/php.ini
 
-    outputHandler "comment" "setting up php-cli.ini"
-    cat /srv/tools/conf-server/php-cli.ini > /etc/php/7.2/cli/php.ini
+    #outputHandler "comment" "setting up php-cli.ini"
+    #cat /srv/tools/conf-server/php-cli.ini > /etc/php/7.2/cli/php.ini
 
 	outputHandler "comment" "Restarting Apache"
 	# RESTART APACHE
