cceb23c0b3311517177b5a56cfc16bd54213f8fd
jessen1989 <jessen-it@outlook.com>
2020-05-14 12:06:36 +0200

    testing deleteAndAppendSection

--- tests/delete_and_append_section.sh
+++ tests/delete_and_append_section.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 source /srv/sites/parentnode/ubuntu_environment/scripts/functions.sh
 
-deleteAndAppendSection "#parentnode: github" "/srv/sites/parentnode/ubuntu_environment/tests/test_update_content/source" "/srv/sites/parentnode/ubuntu_environment/tests/test_update_content/destination"
+deleteAndAppendSection "#section" "/srv/sites/parentnode/ubuntu_environment/tests/test_update_content/source" "/srv/sites/parentnode/ubuntu_environment/tests/test_update_content/destination"
