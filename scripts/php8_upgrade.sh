ccacefa1b97a861fb39264a19bdc4e52417cf1f9
kaestel <martin@think.dk>
2023-08-07 18:49:17 +0200

    Need ppa

--- scripts/php8_upgrade.sh
+++ scripts/php8_upgrade.sh
@@ -11,6 +11,7 @@ outputHandler "comment" "Upgrade to PHP8.2"
 
 
 # INSTALL PHP8.2
+command "sudo add-apt-repository ppa:ondrej/php"
 command "sudo apt-get install -y libapache2-mod-php php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-dev php8.2-mbstring php8.2-zip php8.2-mysql php8.2-xmlrpc"
 
 command "sudo a2dismod php7.4"
