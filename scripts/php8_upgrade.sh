ddc5702208f87480fcd85f1de71ccd9df007f34e
kaestel <martin@think.dk>
2023-08-07 20:00:43 +0200

    Fix permission error

--- scripts/php8_upgrade.sh
+++ scripts/php8_upgrade.sh
@@ -15,8 +15,8 @@ command "sudo apt-get update"
 command "sudo apt-get install -y libapache2-mod-php php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-dev php8.2-mbstring php8.2-zip php8.2-mysql php8.2-xmlrpc"
 
 outputHandler "comment" "setting up php.ini"
-cat /srv/tools/conf-server/php-8.2-apache2.ini > /etc/php/8.2/apache2/php.ini
-cat /srv/tools/conf-server/php-8.2-cli.ini > /etc/php/8.2/cli/php.ini
+command "sudo cat /srv/tools/conf-server/php-8.2-apache2.ini > /etc/php/8.2/apache2/php.ini"
+command "sudo cat /srv/tools/conf-server/php-8.2-cli.ini > /etc/php/8.2/cli/php.ini"
 
 
 command "sudo a2dismod php7.4"
