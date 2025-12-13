469efb8f6bcfa32bf5c7dd517efb17b2d82f5d11
kaestel <martin@think.dk>
2025-12-13 14:20:18 +0100

    Updated for cron action

--- scripts/cron/gdpr_cron_cleaner.sh
+++ scripts/cron/gdpr_cron_cleaner.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
+
+# Crontab
+# 0 0 * * 1 /srv/tools/scripts/cron/gdpr_cron_cleaner.sh >> /srv/crons/gdpr-cron-cleaner/gdpr-cron-$(date +\%Y-\%m-\%d_\%H\%M\%S).log 2>&1
+
+
 CRON_DIR="/srv/crons"
 EXPIRE_DAYS=90
+OUTPUT_DIR="/srv/crons/gdpr-cron-cleaner"
 
 
+# Check if CRON_DIR exists
 if [ ! -d "$CRON_DIR" ]; then
 	echo "Directory not found: $CRON_DIR" >&2
 	exit 1
+fi
+
+
+# Ensure output directory exists
+if [ ! -d "$OUTPUT_DIR" ]; then
+	mkdir -p "$OUTPUT_DIR"
 fi
 
 
--- scripts/cron/gdpr_log_cleaner.sh
+++ scripts/cron/gdpr_log_cleaner.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
+# Crontab
+# 0 0 * * 1 /srv/tools/scripts/cron/gdpr_log_cleaner.sh >> /srv/crons/gdpr-log-cleaner/gdpr-log-$(date +\%Y-\%m-\%d_\%H\%M\%S).log 2>&1
+
+
 SITES_DIR="/srv/sites"
 LOG_DIR="*/theme/library/log"
 EXPIRE_DAYS=180
+OUTPUT_DIR="/srv/crons/gdpr-log-cleaner"
 
 
+# Check if SITES_DIR exists
 if [ ! -d "$SITES_DIR" ]; then
 	echo "Directory not found: $SITES_DIR" >&2
 	exit 1
+fi
+
+
+# Ensure output directory exists
+if [ ! -d "$OUTPUT_DIR" ]; then
+	mkdir -p "$OUTPUT_DIR"
 fi
 
 
@@ -21,3 +33,6 @@ find "$SITES_DIR" -type f -path "$LOG_DIR/*" -mtime +"$EXPIRE_DAYS" -printf '%TY
 # Delete
 #echo "Delete Janitor log files in $SITES_DIR/$LOG_DIR older than $EXPIRE_DAYS days:"
 # find "$SITES_DIR" -type f -path "$LOG_DIR/*" -mtime +"$EXPIRE_DAYS" -print0 | xargs -0 --no-run-if-empty rm -v --
+
+
+#crontab
